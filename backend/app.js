a133e06b23352ca071cf192d0b4cbd1b4e8850dc
Rémi <cabreraremi@gmail.com>
2021-11-12 16:08:44 +0100

    Add routes & controller user

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const app = express();
+const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://remi:motdepasse@piiquante.vrjjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     {
@@ -11,6 +11,9 @@ mongoose.connect('mongodb+srv://remi:motdepasse@piiquante.vrjjm.mongodb.net/myFi
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+
+const app = express();
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -18,7 +21,6 @@ app.use((req, res, next) => {
     next();
 });
 
-
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
-
